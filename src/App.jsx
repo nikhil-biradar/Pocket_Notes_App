d674f4df710ce7e9217c5f28e3b60602e57476fd
Nikhil <nikhilbiradar878@gmail.com>
2024-02-17 15:55:10 +0530

    commit first

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import DesktopView from "./view/DesktopView/DesktopView";
+import MobileView from "./view/MobileView/MobileView";
+import NotesMobilePage from "./components/notesMobilePage/NotesMobilePage";
+
+import { Provider } from "./context/PocketContext";
+import usePocketContext from "./hooks/usePocketContext";
+
+function App() {
+  const [screenSize, setScreenSize] = useState(window.innerWidth);
+  const { selected, setSelected } = usePocketContext();
+
+  useEffect(() => {
+    setSelected(localStorage.getItem("selected") || "");
+  }, [selected]);
+  const checkScreenSize = () => {
+    setScreenSize(window.innerWidth);
+  };
+
+  window.addEventListener("resize", checkScreenSize);
+  return (
+    <Provider>
+      <div className="App">
+        {screenSize > 500 ? (
+          <DesktopView />
+        ) : (
+          <Router>
+            <Routes>
+              <Route path="/" element={<MobileView />} />
+              <Route path="/notes" element={<NotesMobilePage />} />
+            </Routes>
+          </Router>
+        )}
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
